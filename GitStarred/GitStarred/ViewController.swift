156ba34dae9f2f9e898d3977c3669186f9f9ab59
peter <lizhigang19831024@gmail.com>
2018-01-10 13:54:06 +0800

    初始化

---
+++ GitStarred/GitStarred/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitStarred
+//
+//  Created by peter on 2018/1/10.
+//  Copyright © 2018年 l3xlab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
